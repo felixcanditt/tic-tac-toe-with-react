5f371fb7c943c60b754929518dfba10823737a52
Felix Canditt <felix.canditt@gmail.com>
2025-07-29 17:20:32 +0200

    added checkForWinner

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -10,6 +10,17 @@ export default function GameBoard() {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
   const [player, setPlayer] = useState('X');
 
+  function checkForWinner() {
+    const winner = false;
+    console.log(gameBoard[0][0] === gameBoard[0][1]);
+
+    if ((gameBoard[0][0] === gameBoard[0][1]) === gameBoard[0][2]) {
+      console.log(2);
+    }
+  }
+
+  checkForWinner();
+
   function handleClick(rowIndex, columnIndex) {
     setGameBoard((prevGameBoard) => {
       const updatedBoard = [
