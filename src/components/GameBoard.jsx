67b97fc47c6c15910b03b8129b908d971231069c
Felix Canditt <felix.canditt@gmail.com>
2025-07-29 16:44:42 +0200

    button click try out

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,33 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  console.log(1, gameBoard);
+
+  function handleClick(rowIndex, columnIndex) {
+    console.log(5, gameBoard);
+    const oldGameBoard = gameBoard;
+    console.log(10, oldGameBoard);
+
+    oldGameBoard[rowIndex][columnIndex] = 1;
+    console.log(100, oldGameBoard);
+
+    // const updatedGameBoard
+
+    setGameBoard(oldGameBoard);
+    //console.log(3, gameBoard);
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, columnIndex) => (
               <li key={columnIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleClick(rowIndex, columnIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
