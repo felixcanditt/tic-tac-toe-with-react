04f14aff1f7deaf49a18d6c466b1b7f14955c29d
Felix Canditt <felix.canditt@gmail.com>
2025-07-29 13:41:36 +0200

    created Player.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
 import { useState } from 'react';
 
+import Player from './components/Player';
+
 function App() {
-  const [showInput, setShowInput] = useState(false);
-  const [userInput, setUserInput] = useState('');
-  const [playerName, setPlayerName] = useState('Max');
-
-  console.log(userInput);
-
-  function showInputField() {
-    setShowInput(true);
-  }
-
-  function handleInput(e) {
-    const userInput = e.target.value;
-    setUserInput(userInput);
-  }
-
-  function saveInput() {
-    setPlayerName(userInput);
-    setUserInput('');
-    //setShowInput(false);
-  }
-
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">{playerName}</span>
-            <span className="player-symbol">X</span>
-            <button onClick={() => showInputField()}>EDIT</button>
-            <input
-              type="text"
-              value={userInput}
-              onChange={(e) => handleInput(e)}
-            />
-            <button onClick={() => saveInput()}>SAVE</button>
-          </li>
+          <Player />
           <li>
             <span className="player-name">Player 2</span>
             <span className="player-symbol">0</span>
---
+++ src/components/Player.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+export default function Player() {
+  const [showInput, setShowInput] = useState(false);
+  const [userInput, setUserInput] = useState('');
+  const [playerName, setPlayerName] = useState('Max');
+
+  function handleEditClick() {
+    setShowInput(true);
+  }
+
+  function handleInput(e) {
+    const userInput = e.target.value;
+    setUserInput(userInput);
+  }
+
+  function saveInput() {
+    setPlayerName(userInput);
+    setUserInput('');
+    setShowInput(false);
+  }
+  return (
+    <li>
+      <span className="player-name">{playerName}</span>
+      <span className="player-symbol">X</span>
+      <button onClick={() => handleEditClick()}>EDIT</button>
+      {showInput && (
+        <>
+          <input
+            type="text"
+            value={userInput}
+            onChange={(e) => handleInput(e)}
+          />
+          <button onClick={() => saveInput()}>SAVE</button>
+        </>
+      )}
+    </li>
+  );
+}
