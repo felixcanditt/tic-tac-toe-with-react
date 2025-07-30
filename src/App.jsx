cf593255a853327c6cfb6ea6428e244075e331e8
Felix Canditt <felix.canditt@gmail.com>
2025-07-30 14:44:05 +0200

    found isMatch

--- src/App.jsx
+++ src/App.jsx
@@ -6,44 +6,44 @@ import Log from './components/Log';
 
 const WINNING_COMBINATIONS = [
   [
-    { row: 0, col: 0 },
-    { row: 0, col: 1 },
-    { row: 0, col: 2 },
+    { row: 0, column: 0 },
+    { row: 0, column: 1 },
+    { row: 0, column: 2 },
   ],
   [
-    { row: 1, col: 0 },
-    { row: 1, col: 1 },
-    { row: 1, col: 2 },
+    { row: 1, column: 0 },
+    { row: 1, column: 1 },
+    { row: 1, column: 2 },
   ],
   [
-    { row: 2, col: 0 },
-    { row: 2, col: 1 },
-    { row: 2, col: 2 },
+    { row: 2, column: 0 },
+    { row: 2, column: 1 },
+    { row: 2, column: 2 },
   ],
   [
-    { row: 0, col: 0 },
-    { row: 1, col: 0 },
-    { row: 2, col: 0 },
+    { row: 0, column: 0 },
+    { row: 1, column: 0 },
+    { row: 2, column: 0 },
   ],
   [
-    { row: 0, col: 1 },
-    { row: 1, col: 1 },
-    { row: 2, col: 1 },
+    { row: 0, column: 1 },
+    { row: 1, column: 1 },
+    { row: 2, column: 1 },
   ],
   [
-    { row: 0, col: 2 },
-    { row: 1, col: 2 },
-    { row: 2, col: 2 },
+    { row: 0, column: 2 },
+    { row: 1, column: 2 },
+    { row: 2, column: 2 },
   ],
   [
-    { row: 0, col: 0 },
-    { row: 1, col: 1 },
-    { row: 2, col: 2 },
+    { row: 0, column: 0 },
+    { row: 1, column: 1 },
+    { row: 2, column: 2 },
   ],
   [
-    { row: 2, col: 2 },
-    { row: 1, col: 1 },
-    { row: 0, col: 0 },
+    { row: 2, column: 2 },
+    { row: 1, column: 1 },
+    { row: 0, column: 0 },
   ],
 ];
 
@@ -101,6 +101,17 @@ function App() {
         'playerOTurns',
         playerOTurns
       );
+
+      WINNING_COMBINATIONS.map((combination) => {
+        combination.map((square) => {
+          const match = playerXTurns.map((turn) => {
+            const isMatch =
+              turn.row === square.row && turn.column === square.column;
+            console.log(turn, square, isMatch);
+          });
+          console.log(match);
+        });
+      });
     }
   }
 
